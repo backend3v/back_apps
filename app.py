f465ecb8343c0f8f6e74f9f0d8601f0aae3614d6
edwgon15 <111457939+edwgon15@users.noreply.github.com>
2024-04-07 07:52:46 -0500

    requirements

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,jsonify
 import base64
 from aplication.api.routes.test import TestRoutes
 from aplication.api.routes.traslate import TraslateRoutes
+from infrastructure.storage_service import StorageService
 from config import Config
 from flask_cors import CORS
 import firebase_admin
@@ -16,38 +17,17 @@ def hello_world():
 
 
 
+
+
 @app.route('/videos')
 def videos():
-    try:
-        project_folder = os.path.expanduser(os.getcwd())  # adjust as appropriate
-        load_dotenv(os.path.join(project_folder, '.env'))
-        print(f"----{os.getenv('CLIENT_ID')}")
-        config = {
-        "type": "service_account",
-        "project_id": "englishapp-418617",
-        "private_key_id": os.getenv('PRIVATE_KEY_ID'),
-        "private_key": os.getenv('PRIVATE_KEY'),
-        "client_email": "firebase-adminsdk-zn4f0@englishapp-418617.iam.gserviceaccount.com",
-        "client_id": os.getenv('CLIENT_ID'),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zn4f0%40englishapp-418617.iam.gserviceaccount.com",
-        "universe_domain": "googleapis.com"
-        }
-        print(config)
-        cred = credentials.Certificate(config)
-        app = firebase_admin.initialize_app(cred,{'storageBucket': 'englishapp-418617.appspot.com'})# fetch all the files in the bucket
-    except:
-        pass
-    #list(bucket.list_blobs())
-    items = list(firebase_admin.storage.bucket().list_blobs())
-    new_data = []
-    for i in items:
-        new_data.append(str(i.name))
-    print(f"Name Bucket {list(firebase_admin.storage.bucket().list_blobs())}")
-    return jsonify({'data':new_data})
+    return jsonify({"data":StorageService().get_documents()})
 
+@app.route('/video')
+def video():
+    name = "English (auto-generated)] The Big Bang Theory _ Season 1 _ Episode 2 _ The Big Bran Hypothesis [DownSub.com].srt"
+    result = StorageService().get_document(name)
+    return jsonify({"data":result})
 
 class Application:
     def __init__(self):
---
+++ infrastructure/storage_service.py
@@ -0,0 +1,81 @@
+import os
+import firebase_admin
+from firebase_admin import credentials, storage
+import traceback,re
+from dotenv import load_dotenv
+
+class StorageService:
+    def conect(self):
+        try:
+            project_folder = os.path.expanduser(os.getcwd())  # adjust as appropriate
+            load_dotenv(os.path.join(project_folder, '.env'))
+            print(f"----{os.getenv('CLIENT_ID')}")
+            config = {
+            "type": "service_account",
+            "project_id": "englishapp-418617",
+            "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+            "private_key": os.getenv('PRIVATE_KEY'),
+            "client_email": "firebase-adminsdk-zn4f0@englishapp-418617.iam.gserviceaccount.com",
+            "client_id": os.getenv('CLIENT_ID'),
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zn4f0%40englishapp-418617.iam.gserviceaccount.com",
+            "universe_domain": "googleapis.com"
+            }
+            print(config)
+            cred = credentials.Certificate(config)
+            app = firebase_admin.initialize_app(cred,{'storageBucket': 'englishapp-418617.appspot.com'})# fetch all the files in the bucket
+            bucket = firebase_admin.storage.bucket()
+            print(f"Name Bucket {list(bucket.list_blobs())}")
+            return bucket
+        except:
+            print(traceback.format_exc())
+            return None
+    
+    def get_documents(self):
+        bucket = self.conect()
+        items = list(bucket.list_blobs())
+        new_data = []
+        for i in items:
+            new_data.append(str(i.name))
+        return new_data
+    
+    def get_document(self,doc):
+        bucket = self.conect()
+        source_blob_name = "33.txt"
+        blob = bucket.blob(source_blob_name)
+        path = str(os.getcwd())+"/temp.txt"
+        content = None
+        blob.download_to_filename(path)
+        with open(path) as file:
+            content = file.read()
+        dict_result = {}
+        def to_ms(tiempo):
+            hours = int(tiempo[0]) * 3600000
+            minutes = int(tiempo[1]) * 60000
+            seconds = int(tiempo[2]) * 1000
+            miliseconds = seconds + minutes + hours
+            miliseconds += int(tiempo[2]) % 1000
+            miliseconds += int(tiempo[3])
+            return miliseconds
+        def process_match(obj_match):
+            obj = obj_match[0].split(sep="\n")
+            print(obj)
+            time = obj[1].split(sep=" --> ")
+            print(time)
+            time = time[1].replace(",",":")
+            time = time.split(":")
+            print(time)
+            new_list = []
+            for i in time:
+                new_list.append(str(int(i)))
+            time = ":".join(new_list)
+            time = to_ms(new_list)
+            dict_obj ={"end":time,"data":obj[2]}
+            dict_result[obj[0]] = dict_obj
+            return str(obj)
+            
+        patron = r'([0-9]{1,2}\n[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]\s-->\s[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]\n[\w\s]*?[^\n]*)'
+        result = re.sub(patron, process_match,content)
+        return dict_result
